89d8d63989a4dd852c8a96370a64455e7dc6c12e
Marissa <Marissa.Bart@cuw.edu>
2017-03-22 12:23:24 -0500

    Initial Commit

---
+++ TrashCollectorPro/TrashCollectorPro/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TrashCollectorPro.Startup))]
+namespace TrashCollectorPro
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
