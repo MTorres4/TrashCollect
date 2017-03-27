50cf6226a02dd982198fc0fc9c6024be46b5f7a6
Marissa <Marissa.Bart@cuw.edu>
2017-03-27 17:08:17 -0500

    Created a list of addresses based upon zip code and day matching to an employee

--- TrashCollectorPro/TrashCollectorPro/Controllers/EmployeeController.cs
+++ TrashCollectorPro/TrashCollectorPro/Controllers/EmployeeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin.BuilderProperties;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -12,10 +13,14 @@ namespace TrashCollectorPro.Controllers
     public class EmployeeController : Controller
     {
         private ApplicationDbContext db;
+        public List<Addresses> routeAddresses;
+        private int counter;
+        public int Count { get { return counter; } }
         
         public EmployeeController()
         {
             db = new ApplicationDbContext();
+            routeAddresses = new List<Addresses>();
         }
 
         // GET: Employee
@@ -24,15 +29,41 @@ namespace TrashCollectorPro.Controllers
             return View();
         }
 
-        public ActionResult Schedule()
+        public void DetermineIfRouteExists()
         //Need CRUD
         {
             var userRoleId = db.Roles.Where(x => x.Name == "User").First().Id;
             var users = db.Users.Where(x => x.Roles.FirstOrDefault().RoleId == userRoleId).ToList();
             var employeeId = User.Identity.GetUserId();
             var currentEmployeeZip = db.Users.Where(x => x.Id == employeeId).First().ZipCode;
+            var currentEmployeeDay = db.Users.Where(x => x.Id == employeeId).First().CurrentPickUpDay;
 
-            if(users.Select(x => x.ZipCode).Contains(currentEmployeeZip))
+            //if(users.Select(x => x.ZipCode).Contains(currentEmployeeZip) && (users.Select(x => x.CurrentPickUpDay).Contains(currentEmployeeDay)))
+            for (int i = 0; i < users.Count; i++)
+            {
+                //if (users.Select(x => x.ZipCode).Contains(currentEmployeeZip) && (users.Select(x => x.CurrentPickUpDay).Contains(currentEmployeeDay)))
+                if (users[i].ZipCode == currentEmployeeZip && users[i].CurrentPickUpDay == currentEmployeeDay)
+                {
+                    Route(users[i].Id);
+                    counter++;
+                }
+            }
+        }
+
+        public void Route(string userId)
+        {
+            Addresses addresses = new Addresses();
+            addresses.Street = db.Users.Where(x => x.Id == userId).First().Street;
+            addresses.City = db.Users.Where(x => x.Id == userId).First().City;
+            addresses.State = db.Users.Where(x => x.Id == userId).First().State;
+            addresses.ZipCode = db.Users.Where(x => x.Id == userId).First().ZipCode;
+            routeAddresses.Add(addresses);
+        }
+
+        public ActionResult Schedule()
+        {
+            DetermineIfRouteExists();
+            if(counter > 0)
             {
                 return View();
             }
---
+++ TrashCollectorPro/TrashCollectorPro/Models/Addresses.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollectorPro.Models
+{
+    public class Addresses
+    { 
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
+    }
+}
